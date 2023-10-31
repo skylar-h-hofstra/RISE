b75a25fe3b3ca3615c544971ba2ca32522205945
Anoop Krishnadas <anoopkrishnadasgit@gmail.com>
2023-10-31 14:46:37 -0400

    Complete get_price() with Live Feed

--- test_folder/main.py
+++ test_folder/main.py
@@ -18,9 +18,40 @@ def get_price(stock):
     webpage = urlopen(request_site)
     html_byte = webpage.read()
     html = html_byte.decode("utf-8")
-    return html
+    pass_chart(html)
+    #print(html)
+
+def pass_chart(file):
+    d = DummyClass()
+    d.__dict__ = json.loads(file)
+    # print(d.chart)
+    pass_result(d)
+
+def pass_result(d):
+    d2 = DummyClass()
+    d2.__dict__ = d.chart
+    #print(d2.result)
+    pass_meta(d2)
+
+def pass_meta(d2):
+    d3 = DummyClass()
+    d3.__dict__ = d2.result[0]
+    #print(d3.meta)
+    get_regularMarketPrice(d3)
+
+def get_regularMarketPrice(d3):
+    d4 = DummyClass()
+    d4.__dict__ = d3.meta
+    print(d4.regularMarketPrice)
+
+def live_data_feed(stock):
+    while True:
+        get_price(stock)
 
 
+
+class DummyClass:
+    pass
 
 """
 def parse_price():
@@ -31,4 +62,4 @@ def parse_price():
     #return price
 """
 
-print(get_price(stock))
+live_data_feed(stock)
