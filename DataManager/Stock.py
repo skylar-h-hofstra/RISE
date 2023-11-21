e782d8726a6592938a0f372b1e28e4bb966a1993
Anoop Krishnadas <anoopkrishnadasgit@gmail.com>
2023-11-21 13:36:35 -0500

    Refactors Functions into Stock Class

--- DataManager/Stock.py
+++ DataManager/Stock.py
@@ -5,22 +5,13 @@ import test
 class Stock:
     #Constructor
     def __init__(self, ticker):
-        self.ticker = ticker
         self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'}
-        self.url = f"https://finance.yahoo.com/quote/{ticker}?p={ticker}"
-        self.soup = self.get_soup()
-
-
-    def get_soup(self):
-        response = requests.get(self.url, headers=self.headers)
-        return BeautifulSoup(response.text, "html.parser")
-
-
-    def get_stock_price(self):
-        return self.soup.select_one(f'fin-streamer[data-field="regularMarketPrice"][data-symbol="{self.ticker}"]')
-
-    def print_soup(self):
-        print(self.soup)
+        self.update_stock(ticker)
+        
+    def update_stock(self, new_ticker):
+        self.update_ticker(new_ticker)
+        self.update_url()
+        self.update_soup()
 
     def update_ticker(self, new_ticker):
         self.ticker = new_ticker
@@ -31,11 +22,16 @@ class Stock:
     def update_soup(self):
         self.soup = self.get_soup()
 
-    def update_stock(self, new_ticker):
-        self.update_ticker(new_ticker)
-        self.update_url()
-        self.update_soup()
+    def get_soup(self):
+        response = requests.get(self.url, headers=self.headers)
+        return BeautifulSoup(response.text, "html.parser")
 
+    def print_soup(self):
+        print(self.soup)
+
+    def get_stock_price(self):
+        current = self.soup.select_one(f'fin-streamer[data-field="regularMarketPrice"][data-symbol="{self.ticker}"]')
+        return current['value']
 
 
 """ RESERVE: Working Current Price Function
--- DataManager/main.py
+++ DataManager/main.py
@@ -1,5 +1,42 @@
 from Stock import Stock
 
+
+import re
+import json
+from urllib import request
+from urllib.request import Request, urlopen
+import time
+from bs4 import BeautifulSoup
+from openpyxl import load_workbookp
+def parse_graph(ticker):
+    url='https://finance.yahoo.com/quote/'+ticker
+
+    print(url)
+    try:
+        headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+        req = Request(url=url,headers=headers) 
+        resp = urlopen(req)    
+    except:
+        print("no link")
+    html = BeautifulSoup(resp, features="html.parser")
+
+    for row in html.find_all('td'):
+        
+        print(row.text)
+
+
+
+"""
+s = Stock("MSFT")
+s2 = Stock("TSLA")
+
+while(True):
+    print("MSFT: " + s.get_stock_price())
+    print("TSLA: " + s2.get_stock_price())
+"""
+
+
+"""
 s = Stock("MSFT")
 
 def debugging_module(ticker):
@@ -13,6 +50,8 @@ def debugging_module(ticker):
 
 debugging_module("TSLA")
 debugging_module("AAPL")
+"""
+
 
 """s.print_soup()
 
